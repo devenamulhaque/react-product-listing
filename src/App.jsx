45f0ff09dc07e4f36450d0d31d7afba836e19878
Enamul Haque <enamul.iu91@gmail.com>
2022-12-05 01:46:06 +0600

    show api data

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Blog from './components/blog'
 const App = () => {
-  const [blog, setBlog] = useState();
+  const [blog, setBlog] = useState(null);
+  const fetchQuote = async () => {
+    const res = await axios.get('https://fakestoreapi.com/products');
+    setBlog(res.data);
+  }
   useEffect(() => {
-    const fetchQuote = async () => {
-      const res = await axios.get('https://api.quotable.io/random');
-      setBlog(res.data);
-    }
     fetchQuote();
   }, [])
   return (
-    <div className='flex justify-center items-center my-[200px]'>
-
-        <Blog blog={blog}/>
+    <div className='grid grid-cols-3  max-w-[1199px] mx-auto gap-y-16 gap-[20px] my-[100px]'>
+      {
+        blog?.map(single => (
+          <Blog key={single.id} single={single} />
+        ))
+      }
+      
     </div>
   )
 }
--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -1,19 +1,34 @@
 
-const blog = ({blog}) => {
+const blog = ({single}) => {
     
   return (
-    <div>
-        <p>{blog?.author}</p>
-        <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
-            <div className="flex justify-center md:justify-end -mt-16">
-                <img className="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" />
-            </div>
+    <div className="">
+        <div className="w-full h-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between">
             <div>
-                <h2 className="text-gray-800 text-3xl font-semibold">Design Tools</h2>
-                <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
+                <a href="#">
+                    <img className="p-8 rounded-t-lg h-[300px] w-full object-cover" src={single.image} alt="product image" />
+                </a>
+                <div className="px-5">
+                    <a href="#" className="text-[16px] text-gray-800 font-semibold mb-1 block">{single.category}</a>
+                    <a href="#">
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white line-clamp-2">{single.title}</h5>
+                    </a>
+                </div>
+                <div className="flex items-center mt-2.5 mb-5 px-5 pb-5">
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                </div>
             </div>
-            <div className="flex justify-end mt-4">
-                <a href="#" className="text-xl font-medium text-indigo-500">John Doe</a>
+            <div className="px-5 pb-5">
+                <p className="line-clamp-2 mb-4">{single.description}</p>
+                <div className="flex items-center justify-between">
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${single.price}</span>
+                    <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                </div>
             </div>
         </div>
     </div>
