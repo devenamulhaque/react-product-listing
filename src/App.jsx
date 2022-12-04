4e9035c7a9d52d20c0a70c8c6f1effbf225ec8cf
Enamul Haque <enamul.iu91@gmail.com>
2022-12-05 00:01:25 +0600

    api test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import Blog from './components/query'
+import Blog from './components/blog'
 const App = () => {
   const [blog, setBlog] = useState();
   useEffect(() => {
     const fetchQuote = async () => {
-      const res = await axios.get('https://fakestoreapi.com/products');
+      const res = await axios.get('https://api.quotable.io/random');
       setBlog(res.data);
     }
     fetchQuote();
